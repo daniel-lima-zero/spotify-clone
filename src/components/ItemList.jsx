67a6c76b34c73f327812b100cd2b2d0e2cd6b895
Daniel Lima <57147939+daniel-lima-zero@users.noreply.github.com>
2025-02-20 17:39:55 -0300

    artists and songs pages

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import SingleItem from './SingleItem';
-import { Link } from "react-router-dom"; 
+import { Link, useLocation } from "react-router-dom";
 
 const ItemList = ({title, items, itemsArray, path, idPath}) => {
+  const {pathname} = useLocation();
+  const isHome = pathname === '/' 
+  let finalItems =  isHome ? items : Infinity;
 
   return (
     <div className='item-list'>
             <div className='item-list__header'>
                 <h2>{title}</h2>
-                <Link to={path} className='item-list__link' >Show All</Link>
+                {isHome? 
+                  <Link to={path} className='item-list__link' >Show All</Link> 
+                  : <></>
+                }
+                
             </div>
             <div className='item-list__container'>
               {itemsArray.filter((currentValue, index)=>
-                index < items).map((currObj, index) => { 
+                index < finalItems).map((currObj, index) => { 
                   return <SingleItem
                   idPath={idPath}
                   {...currObj}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,12 +3,27 @@ import ItemList from './ItemList'
 import { artistArray } from '../assets/database/artists';
 import { songsArray } from '../assets/database/songs';
 
-const Main = () => {
+const Main = ({type}) => {
   return (
     <div className='main'>
-        <ItemList title="Artists" items={5} itemsArray={artistArray} path='/artists' idPath= "/artist"/>
-
-        <ItemList title="Songs" items={10} itemsArray={songsArray} path='/songs' idPath= "/song"/>
+      {type === 'artists' || type === undefined ? <ItemList 
+          title="Artists" 
+          items={5} 
+          itemsArray={artistArray} 
+          path='/artists' 
+          idPath= "/artist"/>
+        : <></>
+      }
+        
+        {type === 'songs' || type === undefined ? <ItemList 
+          title="Songs" 
+          items={10}
+          itemsArray={songsArray} 
+          path='/songs' 
+          idPath= "/song"/>
+        :<></>
+      }
+        
     </div>
   )
 }
